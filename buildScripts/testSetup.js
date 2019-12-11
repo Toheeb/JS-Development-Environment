eda70775243e2427f09510529ef6f1b6944c7c26
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2019-12-11 12:05:25 +0100

    refactor: start afresh the starter kit
    So far, the commits had been a learning process following Cory House's
    "Building a Javascript Development Environment" on Pluralsight.com.
    
    This time, I had like to build mine as it resonates with me at the moment;
    with only key libraries as needed

--- buildScripts/testSetup.js
+++
@@ -1,7 +0,0 @@
-// This file isn't transpiled. So use CommonJS and Es5
-
-// Register babel to transpile before our tests run
-require('babel-register');
-
-// Disable webpack features that Mocha doesn't understand
-require.extensions['.css'] = function () {};
--- src/vendor.js
+++
@@ -1,5 +0,0 @@
-// Libraries that are used that are supposed to be bundled separately
-
-
-/* eslint-disable no-unused-vars */
-import fetch from 'whatwg-fetch';
