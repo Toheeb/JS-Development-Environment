4f9113d68c43aec471921c46dee6110ff855d9ee
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-01-11 18:18:22 +0100

    perf(bundling): display speed of execution stages
    webpack Speed Measure Plugin is used to display the time it took the
    plugins and loaders to execute the bundling process.
    
    This can be shown by passing the flag ```--env.speed``` to the build script
    
    Example
    ```npm run build -- --env.speed```

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
+
+const smp = new SpeedMeasureWebpackPlugin();
 
 
 module.exports = env => {
 
+  const config = getConfig(env);
+
+  if (env && env.speed) {
+    return smp.wrap(config);
+  }
+
+  return config;
+}
+
+
+function getConfig(env) {
   let mainEntryFile = path.resolve(__dirname, 'src/index.js');
   let outputPath = path.resolve(__dirname, 'dist');
 
