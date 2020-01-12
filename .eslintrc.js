e0200615e73baac9c9fee236a56a5065698c33a4
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-01-12 04:04:52 +0100

    feat(linting): setup javacript linting with eslint
    NB: All failing tests are now passed

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+const editorconfig = require('editorconfig').parseSync('./.editorconfig');
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "indent": ["error", editorconfig.indent_size || 2]
+  },
+};
