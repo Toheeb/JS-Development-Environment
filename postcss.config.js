278cee06efe883b206adc10b2ee169dce5c1f09c
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-01-21 16:44:54 +0100

    feat(optimization): minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 const PostcssPresetEnv = require('postcss-preset-env');
+const CssNano = require('cssnano');
 
 module.exports = {
   plugins: [
     new PostcssPresetEnv({
       stage: 3
+    }),
+    new CssNano({
+      preset: 'default'
     })
   ]
 }
