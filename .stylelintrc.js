ec026a21874c06376b0db9e028f569160caf2166
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-01-22 21:51:53 +0100

    feat(linting): lint stylesheets according to a naming methodology
    Naming methodology could be either BEM, SUIT, or Custom. This is done
    using stylelint and stylelint-selector-bem-pattern

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,14 @@ const editorconfig = require('editorconfig').parseSync('./.editorconfig');
 
 module.exports = {
     "extends": "stylelint-config-standard",
+    "plugins": [
+      "stylelint-selector-bem-pattern"
+    ],
     "rules": {
       "indentation": editorconfig.indent_size || 2,
-      "max-line-length": editorconfig.max_line_length || 80
+      "max-line-length": editorconfig.max_line_length || 80,
+      "plugin/selector-bem-pattern": {
+        "preset": "bem"
+      }
     }
 }
