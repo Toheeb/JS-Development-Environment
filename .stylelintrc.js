45f912b6bad9256567aca98a0c7a4030cbbe2e50
Toheeb Ogunbiyi <toogunbiyi@gmail.com>
2020-01-25 13:36:47 +0100

    feat(linting): show warnings for unsupported styles
    This feature uses a stylelint plugin "stylelint-no-unsupported-browser-features"
    to show warnings for styles not supported by the chosen browsers in
    browserslist configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,13 +3,17 @@ const editorconfig = require('editorconfig').parseSync('./.editorconfig');
 module.exports = {
     "extends": "stylelint-config-standard",
     "plugins": [
-      "stylelint-selector-bem-pattern"
+      "stylelint-selector-bem-pattern",
+      "stylelint-no-unsupported-browser-features"
     ],
     "rules": {
       "indentation": editorconfig.indent_size || 2,
       "max-line-length": editorconfig.max_line_length || 80,
       "plugin/selector-bem-pattern": {
         "preset": "bem"
-      }
+      },
+      "plugin/no-unsupported-browser-features": [true, {
+        "severity": "warning"
+      }],
     }
 }
